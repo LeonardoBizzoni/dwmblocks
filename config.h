ec600dabcaea78ed401edb7eb3f47de2e820ed6e
LeonardoBizzoni <leo2002714@gmail.com>
2020-10-19 18:25:30 +0200

    Changed a bit the bar

--- config.h
+++ config.h
@@ -1,14 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	"weather",	18000,	5 },
-    {"",    "network",  0,      10},
 	{"",	"volume",	0,	    10},
+	{"",	"weather",	0,	    12},
 	{"",	"clock",	60,	    1 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char *delim = " ";
+static char *delim = " | ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
